5697bcec0c43a7fd0ebea40b89b640119c339520
xander <hoffm267@purdue.edu>
2021-10-10 17:09:12 -0400

    bot is now functioning with a few simple response commands

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const { Client, Intents} = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const {token} = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.once('ready', () => {
-    console.log('Ready!');
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
 });
 
 client.login(token);
