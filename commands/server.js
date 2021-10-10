c0feb20d4aafdf333dce9afa95078b56ad008139
xander <hoffm267@purdue.edu>
2021-10-10 17:44:54 -0400

    added ping and server command files in working condition

---
+++ commands/server.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Replies with server info!'),
+    async execute(interaction) {
+        await interaction.reply(`This server's name is ${interaction.guild.name}\nNumber of members: ${interaction.guild.memberCount}`);
+    }
+};
